8f9267dd21f1705cc855f17f86e862151b52c689
Flavio Furlan <furlan.sap@gmail.com>
2009-01-16 23:55:39 -0200

    bug corrections

--- repack.rb
+++ repack.rb
@@ -1,60 +1,68 @@
 require "rubygems"
 require "open4"
 
-# Get the latest sha
-last_sha = File::read( '.last_repack' )
-
 # Get list of all files changes
-if not last_sha == ''
-  cmd = "git diff " + last_sha  + " HEAD --name-status"
-  tar = "tar -cf repack_"+ last_sha +".tar"
+if File.exist?('.last_repack')
+
+  # Get the latest sha
+  last_sha = File::read( '.last_repack' )
+  
+  diff_cmd = "git diff " + last_sha  + " HEAD --name-status"
+  tar_cmd = "tar -cf ../../../repack_"+ last_sha +".tar"
 
   # put first line of manifest
   manifest = last_sha + "\n"
-
+  
+  # tar command prefix
+  tar_cmd = tar_cmd + " reploy_manifest.txt "
+  
 else
   
-  cmd = "git diff  HEAD^1 HEAD --name-status"
-  tar = "tar -cf repack_initial.tar"
+  #cmd = "git diff  HEAD^1 HEAD --name-status"
+  tar_cmd = "tar -cf ../../../repack_initial.tar ../../."
 
   # put first line of manifest
   manifest = "initial\n"
 
 end
 
-pid, stdin, diffout, differr = Open4::popen4 cmd
-
-# tar command prefix
-cmd = tar + " manifest.txt "
-
 puts "Repacking..."
-if last_sha == ''
-  puts "commit: " + last_sha
-else
+
+if diff_cmd.nil?
+
   puts "initial commit"
-end
-
-# include list of files into the tar command
-diffout.each do |file|
-  manifest << file
-  f = file.sub(/[\t]/, '').sub(/[MAD]/, '')
   
-  puts "Repacking file: " + f
+else
+
+  # Executes diff command.
+  pid, stdin, diffout, differr = Open4::popen4 diff_cmd
   
-  f = f.sub(/[\n]/, ' ')
-  cmd <<  f
+  puts "Repacking from commit: " + last_sha
+  
+  # include list of files into the tar command
+  diffout.each do |file|
+    manifest << file
+    f = file.sub(/[\t]/, '').sub(/[MAD]/, '')
+
+    puts "Repacking file: " + f
+
+    f = "../../../" + f.sub(/[\n]/, ' ')
+    tar_cmd <<  f
+  end
+
+  # Manifest contains list of the files.
+  File::open( 'reploy_manifest.txt', 'w' ) do |output_file|
+    output_file << manifest
+  end
+
 end
 
-# Manifest contains list of the files.
-File::open( 'manifest.txt', 'w' ) do |output_file|
-  output_file << manifest
-end
-
-pid, stdin, cmdout, cmderr = Open4::popen4 cmd
+# Executes command tar to generate tar file.
+pid, stdin, cmdout, cmderr = Open4::popen4 tar_cmd
 
 # Update file .last_repack with sha of last commit
-cmd = 'git show'
-pid, stdin, showout, showerr = Open4::popen4 cmd
+show_cmd = 'git rev-parse HEAD'
+pid, stdin, showout, showerr = Open4::popen4 show_cmd
 
 showout.each do |showout_line|
   last_sha = showout_line.sub('commit', '').strip
@@ -64,5 +72,8 @@ end
 File::open( '.last_repack' , 'w') do |last_repack|
   last_repack << last_sha
 end
+
+# Delete reploy_manifest.txt
+system 'rm reploy_manifest.txt'
 
 puts "Repack fineshed successful."
--- reploy.rb
+++ reploy.rb
@@ -1,42 +1,56 @@
 require "rubygems"
-require "open4"
+#require "open4"
+
+repack_file_name = ''
 
 Dir['repack_*.tar'].each do |file_name|
-  cmd = "tar -xf " + file_name
-  #pid, stdin, showout, showerr = Open4::popen4 cmd
+  puts "Reploing file: " + file_name
+  tar_cmd = "tar -xf " + file_name
+  repack_file_name = file_name
+  system tar_cmd
+  #pid, stdin, showout, showerr = Open4::popen4 tar_cmd
   break
 end
 
 j = 0
 
-File.open('manifest.txt', 'r').each do |file_manifest|
-  if j == 0
-    j = j + 1
-    next
-  end
+# If it's a initial deploy, there is no manifest.txt file.
+unless repack_file_name == 'repack_initial.tar'
   
-  f = file_manifest.sub(/[\t]/, ',')
+  File.open('reploy_manifest.txt', 'r').each do |file_manifest|
+    if j == 0
+      j = j + 1
+      next
+    end
   
-  op, file = f.split(',')
+    f = file_manifest.sub(/[\t]/, ',')
   
-  case op
-    when 'A'
-      puts 'Add    : ' + file
-    when 'M'
-      puts 'Modify : ' + file
-    when 'D'
-      cmd = "rm " + file
-      pid, stdin, showout, showerr = Open4::popen4 cmd
+    op, file = f.split(',')
+  
+    case op
+      when 'A'
+        puts 'Add    : ' + file
+      when 'M'
+        puts 'Modify : ' + file
+      when 'D'
+        rm_cmd = "rm " + file
+        #pid, stdin, showout, showerr = Open4::popen4 cmd
+        system rm_cmd
       
-      puts 'Delete : ' + file
-  end
+        puts 'Delete : ' + file
+    end
   
-  j = j + 1
+    j = j + 1
+  end
+
 end
 
+rm_tar_cmd = "rm repack_*.tar"
+#pid, stdin, showout, showerr = Open4::popen4 rm_tar_cmd
+system rm_tar_cmd
 
-cmd = "rm repack_*.tar"
-#pid, stdin, showout, showerr = Open4::popen4 cmd
+# Delete reploy_manifest.txt
+system 'rm reploy_manifest.txt'
 
 puts "Reploy finished successful."
 
